39621472464d2168d98a8f98bc3e44d8aba614e2
futuremouse <kimsoyoonnv@naver.com>
2024-02-11 18:32:45 +0900

    [Feat] 추가 PickerView, TimerView 기능, 추가 TimerRecord CRUD 기능

---
+++ AlarmButler/Timer/Views/TimerLabelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimerLabelTableViewCell.swift
+//  AlarmButler
+//
+//  Created by t2023-m0024 on 2/10/24.
+//
+
+import UIKit
+
+class TimerLabelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
