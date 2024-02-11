cc4f122248f456a837ed337c0ae63380c8fdf0ca
EojinEva <w-w-w-@naver.com>
2024-02-11 17:59:50 +0900

    [Test] SceneDelegate 기존 설정으로 복원, merge 테스트

--- AlarmButler/WorldClock/ViewModels/CoreDataProperties.swift
+++ AlarmButler/WorldClock/ViewModels/CoreDataProperties.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import CoreData
+
+//extension WorldClockEntity {
+//    @nonobjc public override class func fetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+//        return NSFetchRequest<NSFetchRequestResult>(entityName: "WorldClockEntity")
+//    }
+//    
+//    @NSManaged public var date: Date?
+//    @NSManaged public var index: Int64
+//    @NSManaged public var region: String?
+//    @NSManaged public var timeZone: String?
+//}
+//extension WorldClockEntity : Identifiable {
+//    
+//}
