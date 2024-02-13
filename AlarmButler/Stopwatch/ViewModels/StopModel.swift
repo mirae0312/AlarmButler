3d8216bcaf4bf1adca3a1be82aaa77506792d7ac
mysubmin <gnasus2884@naver.com>
2024-02-13 15:05:18 +0900

    [Feat] 스톱워치초기파일생성

--- AlarmButler/Stopwatch/ViewModels/StopModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  StopModel.swift
-//  AlarmButler
-//
-//  Created by t2023-m0039 on 2/13/24.
-//
-
-import Foundation
--- AlarmButler/Stopwatch/Views/StopwatchTableViewCell.swift
+++ AlarmButler/Stopwatch/Views/StopwatchTableViewCell.swift
@@ -1,8 +1,39 @@
 //
-//  StopwatchViewCell.swift
+//  StopwatchTableViewCell.swift
 //  AlarmButler
 //
-//  Created by t2023-m0039 on 2/13/24.
+//  Created by t2023-m0039 on 2/7/24.
 //
 
-import Foundation
+import UIKit
+
+class StopwatchTableViewCell: UITableViewCell {
+    
+    let lapLabel = UILabel()
+    let lapTimer = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        [lapLabel, lapTimer].forEach {
+            contentView.addSubview($0)
+        }
+        
+        lapLabel.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview().inset(10)
+            $0.width.height.equalTo(50)
+        }
+        
+        lapTimer.snp.makeConstraints {
+            $0.centerY.equalTo(lapLabel)
+            $0.leading.equalTo(lapLabel.snp.trailing).offset(10)
+        }
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
